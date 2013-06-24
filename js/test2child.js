e92ecbf2659aa836be35c9b4342b63804f3ac8da
Anthony Bertrand <antho.bertrand101@gmail.com>
2013-06-24 16:50:42 +0200

    add somme awesome affect and  update.

--- js/test2child.js
+++ js/test2child.js
@@ -1,14 +1,28 @@
 !function () {
 	'use strict';
 
+	// add translation
+	var No;
+
 	var TYPE_POOL = 0;
 	var TYPE_HOST = 1;
 	var TYPE_VM   = 2;
 
-	////////////////////////////////////////
 
-	var	w = 1000;
-	var	h = 1000;
+
+	var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
+
+	///////////////////////////////////
+
+	var r = 200;
+	var x = d3.scale.linear().range([0, r]);
+    var y = d3.scale.linear().range([0, r]);
+
+	var	w = winDim().w ;
+	var	h = winDim().h ;
 
 	var link;
 	var node;
@@ -21,9 +35,35 @@
 	;
 
 	var svg = d3.select("body").append("svg")
-		.attr("width", w)
-		.attr("height", h)
+		.attr("object_id","svg")
+		.attr("width", "100%")
+		.attr("height","100%")
+        .attr("viewBox", "0 0 " + w + " " + h )
+        .attr("dblclick",null)
+    ;
+
+	///////////////////////////////////
+
+	svg.on("click",function () { clickdezoom() } )
 	;
+
+	svg
+		.attr("pointer-events", "all")
+		.call(d3.behavior.zoom().on("zoom", function () {
+			graph.attr(
+				"transform",
+				"translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")"
+			);
+		}))
+
+	;
+
+	var graph = svg.append('g')
+	;
+
+	///////////////////////////
+
+	//Force declration
 
 	var tmp = create_nodes_and_links(data);
 	var nodes = tmp[0];
@@ -33,6 +73,11 @@
 	//var links = d3.layout.tree().links(nodes);
 
 	// Adds (hidden) virtual node at the center.
+
+		// Reacts to appropriate events.
+
+
+
 	!function (nodes, links) {
 		var center = {};
 
@@ -47,7 +92,6 @@
 				'distance': 10,
 			});
 		});
-
 
 		force
 			.links(links)
@@ -65,16 +109,18 @@
 		.gravity(0.1)
 	;
 
+
 	update();
 
 	////////////////////////////////////////
 
 	function update()
 	{
+
 		force.start();
 
 		// Updates the links.
-		link = svg.selectAll(".link")
+		link = graph.selectAll(".link")
 			.data(links, function(d, i) {
 				return i;
 			})
@@ -94,7 +140,7 @@
 		link.exit().remove();
 
 		// Update the nodes.
-		node = svg.selectAll(".node")
+		node = graph.selectAll(".node")
 			.data(nodes, function(d, i) {
 				return i;
 			})
@@ -106,10 +152,10 @@
 			.attr("cx", function(d, i) { return d.x; })
 			.attr("cy", function(d) { return d.y; })
 			.attr("r", 15)
-			.attr('fill', function (d) {
+			.attr("fill", function (d) {
 				return colors(d.type);
 			})
-			.call(force.drag)
+			.on("click",function(d,i){ clickelem (d,i) })
 		;
 
 		// Exit any old nodes.
@@ -132,6 +178,81 @@
 			.attr("cy", function(d) { return d.y; })
 		;
 	}
+
+	////////////////////////////////////////
+
+	function clickelem(d,i)
+	{
+
+		if (i  == No )
+		{
+			console.log(4);
+			clickdezoom();
+
+		}
+
+		else
+		{
+
+		var x = (w/2-d.x*4);
+		var y = (h/2-d.y*4);
+
+			graph.transition().duration(200).attr(
+			"transform",
+			"translate("+(x) +","+(y) +") scale(" +4+ ")"
+			);
+		}
+
+		//graph.selectAll("circle").data(nodes, function(d, i != i ) {
+		//		return i ;
+		//	});
+			.attr("fill-opacity",10)
+		;
+
+		No = i ;
+		d3.event.stopPropagation();
+	}
+
+	function clickdezoom()
+	{
+		graph.transition().duration(200).attr(
+			"transform",
+			"translate(0, 0) scale(1)"
+		);
+
+		No = 0 ;
+	}
+
+	////////////////////////////////////////
+
+	/* FONCTION QUI AJUSTE LE SITE AU NAVIGATEUR */
+	function winDim() {
+
+		var W,H,
+			i = window,
+			d = document,
+			de = d.documentElement,
+			db = d.body;
+
+		if ( i.innerWidth ) { // autres que IE
+			W = i.innerWidth;
+			H = i.innerHeight;
+
+		} else if ( de.clientWidth ) { // IE8
+			W = de.clientWidth;
+			H = de.clientHeight;
+
+		}
+
+		else { // IE6
+			W = db.clientWidth;
+			H = db.clientHeight;
+		}
+
+		return {w:W,h:H} ;
+
+
+		}
 
 	////////////////////////////////////////
 
