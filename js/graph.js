9fabe7b04c74cc114021997b244d596fff6036a0
Julien Fontanet <julien.fontanet@isonoe.net>
2013-06-27 17:36:56 +0200

    Major rework.

--- js/graph.js
+++ js/graph.js
@@ -1,318 +1,218 @@
 /**
- * Fonction qui dessine le graphe centré en (0, 0) et s'étendant de [-100, 100] en x et en y.
+ * Fonction qui dessine le graphe centré en (0, 0) et s'étendant de
+ * [-100, 100] en x et en y.
  */
 function molecule_graph(graph, data)
 {
 	'use strict';
 
-	///////////////////////////////////
-	//set graph propreitis
-	var gravity = 0.6;
-	var duration = 400;
-	var mid_x = 500;
-	var mid_y = 300;
-	var rayon = 4 ;
-	var espace = 1 ;
-	var linkdist = 10;
-	var charge = -40;
-	var w = 200;
-	var h = 200;
-	var scale_zoom = 5 ; 				// fixed scale when click on the node
-	///////////////////////////////////
+	//--------------------------------------
 
-	var save_translate = 0 ;
-	var save_scale = 1 ;	// save the position for p.
-	var No; 							// need to save past number of node.
-	var activate; 					    // solve the problem with zoom and move.
+	graph = graph.append('g')
+		.attr('pointer-events', 'all')
+
+		// @todo Not sure it is very clean to use a nested element as
+		// a clip path.
+		.attr('clip-path', 'url(#molecule-graph-area)')
+	;
+
+	//--------------------------------------
+
+	// Clipping element.
+	graph.append('defs').append('clipPath')
+		.attr('id', 'molecule-graph-area')
+		.append('rect')
+			.attr('x', -100)
+			.attr('y', -100)
+			.attr('width', 200)
+			.attr('height', 200)
+	;
+
+	// Background element which allows us to get pointer events.
+	var area = graph.append('rect')
+		.attr('x', -100)
+		.attr('y', -100)
+		.attr('width', 200)
+		.attr('height', 200)
+		.attr('fill', 'white')
+		.attr('stroke', 'black')
+	;
+
+	//--------------------------------------
+
+	var group = graph.append('g')
+		.attr('transform', 'translate(-100, -100)') // Initial translation.
+	;
+
+	//--------------------------------------
+
+	var zoomer = d3.behavior.zoom()
+		.translate([-100, -100]) // Initial translation.
+		.on('zoom', function () {
+			var e = d3.event;
+
+			// @todo When we are focused on a node.
+
+			group.attr(
+				'transform',
+				'translate('+ e.translate +') scale('+ e.scale +')'
+			);
+		})
+	;
+
+	graph
+		.call(zoomer)
+		.on('dblclick.zoom', null)
+	;
+
+	graph
+		.on('click', previous_view)
+		.on('dblclick', reset_view)
+	;
+
+	//======================================
 
 	var TYPE_POOL = 0;
 	var TYPE_HOST = 1;
 	var TYPE_VM   = 2;
 
+	// Pools to center distance.
+	var pool_distance = 10;
 
+	// Hosts to pools distance.
+	var host_distance = 10;
 
-	var link;
+	// VMs to hosts distance.
+	var vm_distance = 10;
+
+	// Node radius (can be a function).
+	var node_radius = 4;
+
+	// Node color (can be a function).
+	var node_color = function (node) {
+		var colors = ['#1f77b4', '#ff7f0e', '#2ca02c'];
+		return colors[node.type];
+	};
+
+	// Animation duration.
+	var duration = 400;
+
+	//--------------------------------------
+
+	// @todo
 	var node;
+	var link;
 
-	var colors = d3.scale.category10();
+	var current_node = null;
+
+	//--------------------------------------
 
 	var force = d3.layout.force()
-		.on("tick", tick)
-		.size([w, h])
-		;
-
-	var svgclip= graph.append("defs").append("clipPath")
-        .attr("id", "clip")
-        .append("rect")
-        .attr("id", "clip-rect")
-		.attr("width",w)
-		.attr("height",h)
-
-	var move_graph = graph.append("g")
-		.attr("clip-path", "url(#clip)")
-			move_graph.attr(
-							"transform",
-							"translate("+mid_x+","+mid_y+")");
-
-    var rect = move_graph.append("g").append('rect')
-        .attr('width', w)
-        .attr('height', h)
-        .attr('fill', 'white')
-        .attr("stroke","black");
-
-	var selection= move_graph.append("g")
-
-	///////////////////////////////////
-	// SVG INTERACTIVITY
-	move_graph.on("dblclick", reset_view);
-	move_graph.on("click", previous_view);
-
-	var zoom = d3.behavior.zoom();
-
-	move_graph.call(zoom.on("zoom", function () {
-				if (activate)
-				{
-					reset_view();
-				}
-				selection.attr(
-					"transform",
-				"translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")"
-				);
-				save_translate = d3.event.translate ;
-				save_scale = d3.event.scale ;
-
-			}))
-	        .on("dblclick.zoom", null)
-		;
-
-	///////////////////////////
-	//Force declration
-
-	var tmp = create_nodes_and_links(data);
-	var nodes = tmp[0];
-	var links = tmp[1];
-
-	// Creates a new tree layout with the default settings.
-	//var links = d3.layout.tree().links(nodes);
-
-	// Adds (hidden) virtual node at the center.
-
-	// Reacts to appropriate events.
-
-	!function (nodes, links) {
-		var center = {};
-
-		nodes = nodes.slice(0);
-		links = links.slice(0);
-
-		nodes.push(center);
-		_.each(data, function (pool) {
-			links.push({
-				'source': center,
-				'target': pool,
-				'distance': 10,
-			});
-		});
-		force
-			.links(links)
-			.nodes(nodes)
-		;
-	}(nodes, links);
-
-	force
-		// A negative value results in node repulsion, while a
-		// positive value results in node attraction.
-		.charge(charge)
+		.size([200, 200])
+		.charge(-40)
 		.linkDistance(function (link) {
-			return link.distance || linkdist;
+			return link.distance || 10;
 		})
-		.gravity(gravity)
+		.gravity(0.6)
+		.on('tick', function () {
+			node.attr(
+				'transform',
+				function (d) {
+					return "translate("+ d.x +","+ d.y +")";
+				}
+			);
+
+			link
+				.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; })
+			;
+		})
 	;
+
 	update();
-	////////////////////////////////////////
+
+	//--------------------------------------
 
 	function update()
 	{
+		// @todo Find a way to remove “tmp”.
+		var tmp = create_nodes_and_links(data);
+		var nodes = tmp[0];
+		var links = tmp[1];
+
+		// Creates a virtual node at the center.
+		!function (nodes, links) {
+			var center = {};
+
+			nodes = nodes.slice();
+			links = links.slice();
+
+			nodes.push(center);
+			_.each(data, function (pool) {
+				links.push({
+					'source': center,
+					'target': pool,
+					'distance': 10,
+				});
+			});
+			force
+				.links(links)
+				.nodes(nodes)
+			;
+		}(nodes, links);
+
 		force.start();
 
-		// Updates the links.
-		link = selection.selectAll(".link")
-			.data(links, function(d, i) {
-				return i;
-			})
+		//------------------
+
+		link = group.selectAll('.link').data(links, function (link, i) {
+			// @todo
+			return i;
+		});
+
+		link.enter().append('line')
+			.attr('class', 'link')
+			.attr('stroke', 'black') // @todo CSS.
 		;
 
-		// Enter any new links.
-		link.enter().insert("line", ".link")
-
-			.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; })
-			.attr('stroke', 'black')
-			.classed("link")
-		;
-
-		// Exit any old links.
 		link.exit().remove();
 
+		//------------------
 
-		// Marche à suivre :
-		// 1. Sélectionner les nodes (groupes avec la classe "node") existants.
-		// 2. Pour les nodes manquants (sélection "enter") :
-		//    1. Créer un groupe avec la classe "node".
-		//    2. Créer le circle avec la bonne couleur.
-		//    3. Créer le texte avec le label.
-		// 3. Pour les nodes en trop (sélection "exit") : les supprimer.
-		// 4. Pour tous les nodes existants : mettre à jour leur coordonnées (attribut "transform").
+		node = group.selectAll('.node').data(nodes, function (link, i) {
+			// @todo
+			return i;
+		});
 
-		// Update the nodes.
-		node = selection.selectAll(".node")
-			.data(nodes, function(d, i) {
-				return i;
-			})
-		;
-
-		//create groups for each node
-		var new_node  = node.enter()
-			.append("g")
-			.classed("node",true);
-
-
-		new_node
-			.append("circle")
-			.attr("r", rayon)
-			.attr("fill", function (d) {
-				return colors(d.type);
-			})
-			.on("click", node_view)
-		;
-
-
-		new_node.append("text")
-			.text(function(d) { return d.label})
-			.on("click", node_view)
-		;
-
-		// Exit any old nodes.
-		node.exit().remove();
-	}
-
-	// END OF UPDATE
-	////////////////////////////////////////
-	// TICK FUNCTION
-
-	function tick()
-	{
-		link
-			.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; })
-		;
-
-		node
+		var new_node = node.enter().append('g')
+			.attr('class', 'node')
+			.on('click', select_node)
 			.attr(
 				"transform",
 				function (d, i) {
 					return "translate("+ d.x +","+ d.y +")";
 				}
-			);
+			)
 		;
+
+		new_node.append('circle')
+			.attr('r', node_radius)
+			.attr('fill', node_color)
 		;
+
+		// @todo Shift position.
+		new_node.append('text')
+			.text(function (d) {
+				return d.label;
+			})
+		;
+
+		node.exit().remove();
 	}
-	//END OF TICK
-	////////////////////////////////////////
-	// POINTER EVENT FUNCTION
 
-	function node_view (d,current)
-	{
-		if (current == No)
-		{
-			previous_view();
-		}
-		else
-		{
-			node.classed("selected", function (d, i) {
-				return (i === current);
-			});
-
-			var x = (w/2-d.x*scale_zoom);
-			var y = (h/2-d.y*scale_zoom);
-
-			selection.transition().duration(duration).attr(
-				"transform",
-				"translate("+(x) +","+(y) +") scale(" +scale_zoom+ ")"
-			);
-			No = current ;
-
-			node.select("circle").transition().duration(duration).style("fill", function (d, i) {
-				if (current === i)
-				{
-					return null;
-				}
-				var old = d3.rgb(d3.select(this).attr("fill"));
-				var bg = d3.rgb('white');
-
-				return d3.rgb(
-					old.r * 0.1 + bg.r * 0.9,
-					old.g * 0.1 + bg.g * 0.9,
-					old.b * 0.1 + bg.b * 0.9
-				).toString();
-			});
-
-			link.transition().duration(duration).style("stroke-opacity",0.1);
-
-			d3.event.stopPropagation();// stop to go back in the SVG DOM.
-		}
-		activate = true ;
-	}
-	// END OF NODE_VIEW
-	////////////////////////////////////////
-	// RESET THE VIEW
-
-	function reset_view()
-	{
-		node.classed("selected", false);
-
-		selection.transition().duration(duration).attr(
-			"transform",
-			"translate(0, 0)"
-		);
-
-		node.select("circle").transition().duration(duration).style("fill", null)
-		link.transition().duration(duration).style("stroke-opacity",1);
-
-		zoom.translate([mid_x, mid_y]);
-		zoom.scale(1);
-
-		activate = false ;
-
-		No = 0;
-	}
-	// END OF RESET_VIEW
-	////////////////////////////////////////
-	// BACK TO THE PAST VIEW BEFORE ZOOM ON NODE
-
-	function previous_view()
-	{
-		activate = false ;
-
-		node.classed("selected", false);
-
-		selection.transition().duration(duration).attr(
-			"transform",
-			"translate(" + save_translate + ") scale(" + save_scale + ")"
-		);
-
-		node.select("circle").transition().duration(duration).style("fill", null)
-		link.transition().duration(duration).style("stroke-opacity",1);
-
-		No = 0 ;
-	}
-	// END OF PREVIOUS_VIEW
-	////////////////////////////////////////
-
+	//--------------------------------------
 
 	function create_nodes_and_links(pools)
 	{
@@ -334,16 +234,18 @@ function molecule_graph(graph, data)
 				links.push({
 					'source': host,
 					'target': pool,
+					'distance': host_distance,
 				});
 
 				_.each(host.vms, function (vm) {
 					vms.push(vm);
 
-					vms.type = TYPE_VM;
+					vm.type = TYPE_VM;
 
 					links.push({
 						'source': vm,
 						'target': host,
+						'distance': vm_distance,
 					});
 				});
 			});
@@ -352,29 +254,32 @@ function molecule_graph(graph, data)
 		// Initially places pools, hosts and VMs on concentric
 		// circles.
 
-		var mid_h = h / 2;
-		var mid_w = w / 2;
+		var mid_h = 100;
+		var mid_w = 100;
 		var TWO_PI = 2 * Math.PI;
 
 		var alpha = TWO_PI / pools.length;
+		var radius = pool_distance;
 		_.each(pools, function (pool, i) {
 			var alpha_i = alpha * i;
-			pool.x = Math.cos(alpha_i) * espace + mid_w;
-			pool.y = Math.sin(alpha_i) * espace + mid_h;
+			pool.x = Math.cos(alpha_i) * radius + mid_w;
+			pool.y = Math.sin(alpha_i) * radius + mid_h;
 		});
 
 		var alpha = TWO_PI / hosts.length;
+		var radius = pool_distance + host_distance;
 		_.each(hosts, function (host, i) {
 			var alpha_i = alpha * i;
-			host.x = Math.cos(alpha_i) + espace * 2 + mid_w;
-			host.y = Math.sin(alpha_i) + espace * 2  + mid_h;
+			host.x = Math.cos(alpha_i) + radius + mid_w;
+			host.y = Math.sin(alpha_i) + radius + mid_h;
 		});
 
 		var alpha = TWO_PI / vms.length;
+		var radius = pool_distance + host_distance + vm_distance;
 		_.each(vms, function (vm, i) {
 			var alpha_i = alpha * i;
-			vm.x = Math.cos(alpha_i) + espace * 3 + mid_w;
-			vm.y = Math.sin(alpha_i) + espace * 3 + mid_h;
+			vm.x = Math.cos(alpha_i) + radius + mid_w;
+			vm.y = Math.sin(alpha_i) + radius + mid_h;
 		});
 
 		return [
@@ -382,9 +287,103 @@ function molecule_graph(graph, data)
 			links
 		];
 	}
-	// END OF create_nodes_and_links
-	////////////////////////////////////////
 
+	//--------------------------------------
+
+	function select_node(d, current_node_i)
+	{
+		if (current_node === this)
+		{
+			previous_view();
+			return;
+		}
+
+		// @todo Mutualize.
+		if (current_node)
+		{
+			d3.select(current_node).classed('selected', false);
+		}
+
+		// Marks this node as selected.
+		current_node = this;
+		d3.select(this).classed('selected', true);
+
+		// @todo Fade any other links and node.
+		node.select('circle').transition().duration(duration).style('fill', function (d, i) {
+			if (current_node_i === i)
+			{
+				return null;
+			}
+			var old = d3.rgb(d3.select(this).attr('fill'));
+			var bg = d3.rgb('white');
+
+			return d3.rgb(
+				old.r * 0.1 + bg.r * 0.9,
+				old.g * 0.1 + bg.g * 0.9,
+				old.b * 0.1 + bg.b * 0.9
+			).toString();
+		});
+		link.transition().duration(duration).style('stroke-opacity', 0.1);
+
+		// Zooms on the selected node.
+		var scale = 5;
+		var x = -d.x * scale;
+		var y = -d.y * scale;
+		group.transition().duration(duration).attr(
+			'transform',
+			'translate('+ x +','+ y +') scale('+ scale +')'
+		);
+
+		// Prevents the event from being re-catched by “graph”.
+		d3.event.stopPropagation();
+	}
+
+	//--------------------------------------
+
+	function reset_view()
+	{
+		// @todo Mutualize.
+		if (current_node)
+		{
+			d3.select(current_node).classed('selected', false);
+			current_node = null;
+
+			node.select('circle').transition().duration(duration).style('fill', function() {
+				return d3.select(this).attr('fill');
+			});
+			link.transition().duration(duration).style('stroke-opacity', 1);
+		}
+
+		group.transition().duration(duration).attr(
+			'transform',
+			'translate(-100, -100)'
+		);
+
+		zoomer.translate([-100, -100]);
+		zoomer.scale(1);
+	}
+
+	//--------------------------------------
+
+	function previous_view()
+	{
+		// @todo Mutualize.
+		if (current_node)
+		{
+			d3.select(current_node).classed('selected', false);
+			current_node = null;
+
+			node.select('circle').transition().duration(duration).style('fill', function() {
+				return d3.select(this).attr('fill');
+			});
+			link.transition().duration(duration).style('stroke-opacity', 1);
+		}
+
+		group.transition().duration(duration).attr(
+			'transform',
+			'translate('+ zoomer.translate().join(',') +') scale('+ zoomer.scale() +')'
+		);
+	}
 }
 
 
@@ -392,60 +391,21 @@ function molecule_graph(graph, data)
 !function () {
 	'use strict';
 
+	var width = 500;
+	var height = 500;
 
-	///////////////////////////////////
-	// keep page propreyty.
-	var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0];
-	///////////////////////////////////
+	var svg = d3.select('body').append('svg')
+		.attr('width', width)
+		.attr('height', height)
+	;
 
-	var	w = winDim().w ;
-	var	h = winDim().h ;
-
-
-	var svg  = d3.select("body").append("svg")
-		.attr("width", "100%")
-		.attr("height","100%")
-		.attr("viewBox", "0 0 " + w + " " + h )
-        .attr("pointer-events", "all")
-    ;
-
-    var graph = svg.append("g")
-
-	// SIZE THE WINDOW AT THE BEGINING
-
-	function winDim() {
-
-		var W,H,
-			i = window,
-			d = document,
-			de = d.documentElement,
-			db = d.body;
-
-		if ( i.innerWidth ) { // other of IE
-			W = i.innerWidth;
-			H = i.innerHeight;
-
-		} else if ( de.clientWidth ) { // IE8
-			W = de.clientWidth;
-			H = de.clientHeight;
-
-		}
-
-		else { // IE6
-			W = db.clientWidth;
-			H = db.clientHeight;
-		}
-
-		return {w:W,h:H} ;
-
-
-	}
-	// END OF WINDIM
-	////////////////////////////////////////
+	var graph = svg.append('g')
+		.attr(
+			'transform',
+			'translate('+ width/2 +','+ height/2 +') scale('+ width/200 +')'
+		)
+	;
 
 	// Dessine le graphe dans l'élément graph.
-	molecule_graph(svg, data);
+	molecule_graph(graph, data);
 }();
